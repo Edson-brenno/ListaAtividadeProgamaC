783f0cab3e3535ea0fa64296775f50b1b00dbd1c
brenno <brennorebeiro@gmail.com>
2024-03-10 20:47:51 -0300

    Finish questao 7

--- questoesListaDeExercicio/questao_sete.h
+++ questoesListaDeExercicio/questao_sete.h
@@ -2,8 +2,10 @@
 #define QUESTAO_SETE_H
 
 #include <stdio.h>
+#include <math.h>
 #include "../utilitario/utilitarios.h"
 
+// Pergunta o coeficiente ao usuario;Ask's the user for the equation
 void pergunta_coeficiente_da_equacao(char *coeficiente, double *variavelCoeficiente){
     
     while(true){
@@ -25,8 +27,6 @@ void pergunta_coeficiente_da_equacao(char *coeficiente, double *variavelCoeficie
             continue; //Start over the while
         }
 
-        free(coeficiente);
-        free(variavelCoeficiente);
         break;
     }
     
@@ -34,13 +34,32 @@ void pergunta_coeficiente_da_equacao(char *coeficiente, double *variavelCoeficie
 
 void resolucao_questao_sete(){
     
+    //Variaveis
     double a,b,c,delta;
 
+    // Pergunta o usuario;Ask's the user
     pergunta_coeficiente_da_equacao("a", &a);
     pergunta_coeficiente_da_equacao("b", &b);
     pergunta_coeficiente_da_equacao("c", &c);
 
-    //delta = b ^ 2;
+    //Calcula delta; Calc delta
+    delta = pow(b,2) - 4 * (a * c);
 
+    if(delta > 0){
+        double x1 = ((-b + sqrt(delta)) / (2 * a));
+        double x2 = ((-b - sqrt(delta)) / (2 * a));
+
+        printf("======================================================\n");
+        printf("Duas solucoes reais: x1 = %.2lf e x2 = %.2lf \n", x1,x2);
+    }
+    else if(delta == 0){
+        double x = -b / (2 * a);
+
+        printf("======================================================\n");
+        printf("uma solução real: x = %.2lf \n", x);
+    }
+    else{
+        printf("Não possui solução real");
+    }
 }
 #endif
