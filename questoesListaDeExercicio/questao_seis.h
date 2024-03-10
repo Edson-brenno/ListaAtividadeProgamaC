819ea7f3fc1777d774a7d67af8ee22b9484965a4
brenno <brennorebeiro@gmail.com>
2024-03-10 19:41:11 -0300

    Finish resolucao questao 6

--- questoesListaDeExercicio/questao_seis.h
+++ questoesListaDeExercicio/questao_seis.h
@@ -2,8 +2,40 @@
 #define QUESTAO_SEIS_H
 
 #include <stdio.h>
+#include <math.h>
+#include "../utilitario/utilitarios.h"
 
 void resolucao_questao_seis(){
-    printf("oi");
+    
+    //variaveis
+    int numero;
+
+    // enquanto o numero não for valido; While the number is not valid
+    while(true){
+        
+        int isAValidDigit = 1; 
+
+        printf("Digite um numero inteiro: ");
+
+        if(scanf("%d", &numero) != 1){ // se não for um input valido; If is not a valid input
+            isAValidDigit = 0;
+
+            limpa_input_buffer(); // clear input buffer
+        }
+
+        if(isAValidDigit == 0){ // mostra mensagem error se o input não foi valido;show error if the input was not valid
+            
+            limpar_tela(); //limpa tela;Clear screen
+
+            printf("Error! Nao pode ser diferente de inteiro\n"); //Mostra mensagem error; Show's error mensage
+
+            continue; //Start over the while
+        }
+
+        break; // Stop the while
+    }
+    
+    printf("===================================================================================\n");
+    printf("O antecessor de %d e %d; O sucessor de %d e %d", numero, numero - 1, numero, numero + 1);
 }
 #endif
