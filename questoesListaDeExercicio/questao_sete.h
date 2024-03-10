e40ee85a0f3fb93f606113692e18cf129e7f6f13
brenno <brennorebeiro@gmail.com>
2024-03-10 20:16:04 -0300

    Add pergunta coreficiente

--- questoesListaDeExercicio/questao_sete.h
+++ questoesListaDeExercicio/questao_sete.h
@@ -4,7 +4,43 @@
 #include <stdio.h>
 #include "../utilitario/utilitarios.h"
 
+void pergunta_coeficiente_da_equacao(char *coeficiente, double *variavelCoeficiente){
+    
+    while(true){
+
+        int isAValidDigit = 1;
+
+        printf("Digite o coeficiente %s da equacao: ", coeficiente);
+
+        if(scanf("%lf", variavelCoeficiente) != 1){
+            isAValidDigit = 0;
+            limpa_input_buffer();
+        }
+
+        if(isAValidDigit == 0){
+            limpar_tela(); //limpa tela;Clear screen
+
+            printf("Error! Nao pode ser diferente de um numero\n"); //Mostra mensagem error; Show's error mensage
+
+            continue; //Start over the while
+        }
+
+        free(coeficiente);
+        free(variavelCoeficiente);
+        break;
+    }
+    
+}
+
 void resolucao_questao_sete(){
-    printf("chegamos na sete");
+    
+    double a,b,c,delta;
+
+    pergunta_coeficiente_da_equacao("a", &a);
+    pergunta_coeficiente_da_equacao("b", &b);
+    pergunta_coeficiente_da_equacao("c", &c);
+
+    //delta = b ^ 2;
+
 }
 #endif
