a7f537f77fd3e05267dfe35bf9c4159844220e63
brenno <brennorebeiro@gmail.com>
2024-03-10 22:04:46 -0300

    Finish project

--- main.c
+++ main.c
@@ -1,22 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "lista_exercicio.h"
-#include "questoesListaDeExercicio/questao_um.h"
-#include "questoesListaDeExercicio/questao_dois.h"
-#include "questoesListaDeExercicio/questao_tres.h"
-#include "questoesListaDeExercicio/questao_quatro.h"
-#include "questoesListaDeExercicio/questao_cinco.h"
-#include "questoesListaDeExercicio/questao_seis.h"
-#include "questoesListaDeExercicio/questao_sete.h"
 #include "menu.h"
 
-
 int main(){
-    
-    // ResolucaoListaExercicio resolucaoListaExercicio = {&questao_um,&resolucao_questao_dois,&resolucao_questao_tres,
-    // &resolucao_questao_quatro, &resolucao_questao_cinco, &resolucao_questao_seis, &resolucao_questao_sete};
-
-    // resolucaoListaExercicio.resolucao_questao_tres();
 
     roda_menu();
 
