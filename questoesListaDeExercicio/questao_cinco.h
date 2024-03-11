ea4ad456ff4fbb1375467836804fdf96c943e658
brenno <brennorebeiro@gmail.com>
2024-03-10 21:07:22 -0300

    Refatora questao cinco

--- questoesListaDeExercicio/questao_cinco.h
+++ questoesListaDeExercicio/questao_cinco.h
@@ -4,69 +4,45 @@
 #include <stdio.h>
 #include <math.h>
 
+void pergunta_pela_nota(char *classificacaoNota, double *variavelNota){
+    
+    while (true){ //Enquanto o primeiro nota não for valido; While the first number is not valid
+
+        int isNotAValidDigit = 0; // verifica se é um nota valido;check if is a valid digit
+
+        printf("Digite a %s nota: ", classificacaoNota);
+
+        if(scanf("%lf", variavelNota) != 1){ // se o input não for valido; if the input is not valid
+            isNotAValidDigit = 1; 
+            limpa_input_buffer(); // limpa input buffer
+        };
+        
+        if(isNotAValidDigit == 1){ // Se o nota digitado for diferente de um nota; if the typed name is different than a number
+            limpar_tela(); // limpa tela; clear screen
+
+            printf("Error! Nao pode ser diferente de numero \n"); // mostra mensagem error; Print error mensage
+            continue;  //start over the while      
+        }
+        else if(isless(*variavelNota, 0)){
+            
+            limpar_tela(); // limpa tela; clear screen
+
+            printf("Error! Nao pode ser menor que zero \n"); // mostra mensagem error; Print error mensage
+            continue;  //start over the while      
+        }
+
+        break;
+    }
+}
+
 void resolucao_questao_cinco(){
     
     //variaveis
     double notaUm = 0.0,notaDois = 0.0; 
 
-    while (true){ //Enquanto o primeiro nota não for valido; While the first number is not valid
+    pergunta_pela_nota("primeira", &notaUm);
+    pergunta_pela_nota("segunda", &notaDois);
 
-        int isNotADigit = 0; // verifica se é um nota valido;check if is a valid digit
-
-        printf("Digite a primeira nota: ");
-        if(scanf("%lf", &notaUm) != 1){ // se o input não for valido; if the input is not valid
-            isNotADigit = 1; 
-            limpa_input_buffer(); // limpa input buffer
-        };
-        
-        if(isNotADigit == 1){ // Se o nota digitado for diferente de um nota; if the typed name is different than a number
-            limpar_tela(); // limpa tela; clear screen
-
-            printf("Error! Nao pode ser diferente de numero \n"); // mostra mensagem error; Print error mensage
-            continue;  //start over the while      
-        }
-        else if(isless(notaUm, 0)){
-            
-            limpar_tela(); // limpa tela; clear screen
-
-            printf("Error! Nao pode ser menor que zero \n"); // mostra mensagem error; Print error mensage
-            continue;  //start over the while      
-        }
-
-        break;
-    }
-    
-    while(true){ //Enquanto o segundo nota não for valido; While the second number is not valid
-
-        int isNotADigit = 0;
-
-        printf("Digite a segunda nota: ");
-        
-         if(scanf("%lf", &notaDois) != 1){ // se o input não for valido; if the input is not valid
-            isNotADigit = 1; 
-            limpa_input_buffer(); // limpa input buffer
-        };
-        
-        if(isNotADigit == 1){ // Se o nota digitado for diferente de um nota; if the typed name is different than a number
-            
-            limpar_tela(); // limpa tela; clear screen
-
-            printf("Error! Nao pode ser diferente de numero \n"); // mostra mensagem error; Print error mensage
-            
-            continue;  //start over the while     
-        }
-        else if(isless(notaDois, 0)){
-            
-            limpar_tela(); // limpa tela; clear screen
-
-            printf("Error! Nao pode ser menor que zero \n"); // mostra mensagem error; Print error mensage
-            
-            continue;  //start over the while      
-        }
-
-        break;
-    }
-
-    printf("A media do aludo foi %.2lf", (notaUm + notaDois) / 2);
+    printf("A media do aluno foi %.2lf", (notaUm + notaDois) / 2);
 }
 #endif
